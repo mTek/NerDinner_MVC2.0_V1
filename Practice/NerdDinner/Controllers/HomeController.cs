bfd47be8c153e4930dffbb523e42d10105f1fd27
mtek <mtek.helpdesk@gmail.com>
2012-08-23 17:50:07 +0100

    Practice Project Added

---
+++ Practice/NerdDinner/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NerdDinner.Models;
+using System.Data.Entity;
+namespace NerdDinner.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            NerdDinnerEntities entities = new NerdDinnerEntities();
+            var udinners = from dinner in entities.Dinners
+                             where dinner.EventDate > DateTime.Now
+                             orderby dinner.EventDate
+                             select dinner;
+            string strDinners = "";
+            foreach (Dinner dinner in udinners)
+            {
+                if (strDinners.Trim() == "")
+                {
+                    strDinners = dinner.Title;
+                }
+                else
+                {
+                    strDinners = strDinners + dinner.Title;
+                }
+            }
+            ViewData["Dinners"] = strDinners;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
