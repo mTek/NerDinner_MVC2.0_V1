a112395c7033b3f6a07df4746c89d4f5d28161fb
M Tek <mtek.helpdesk@gmail.com>
2012-09-05 00:24:00 +0100

    home

--- Practice2/NerdDinner/NerdDinner/Controllers/DinnersController.cs
+++ Practice2/NerdDinner/NerdDinner/Controllers/DinnersController.cs
@@ -18,10 +18,47 @@ namespace NerdDinner.Controllers
             return View(dinnerRepository.FindUpcomingDinners());
         }
 
+        //
+        // GET: /Dinners/Details/2
+
         public ActionResult Details(int id)
         {
             return View(dinnerRepository.GetDinner(id));
         }
+
+        //
+        // GET: /Dinners/Details/2
+
+        public ActionResult Edit(int id)
+        {
+            return View(dinnerRepository.GetDinner(id));
+        }
+
+        //
+        // POST: /Dinners/Details/2
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection form)
+        {
+            Dinner dinner = dinnerRepository.GetDinner(id);
+            if (TryUpdateModel(dinner))
+            {
+                dinnerRepository.Save();
+                return RedirectToAction("Details", new { id=dinner.DinnerID });
+            }
+            return View(dinner);
+
+            //bool result = TryUpdateModel(dinner);
+            //if (result)
+            //{
+            //    dinnerRepository.Save();
+            //    return View("Details", dinner);
+            //}
+            //else
+            //{
+            //    return View("Error");
+            //}
+        }
+
 
     }
 }
---
+++ Practice2/NerdDinner/NerdDinner/Models/Dinner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace NerdDinner.Models
+{
+    [MetadataType(typeof(Dinner_Validation))]
+    public partial class Dinner
+    {
+    }
+    public class Dinner_Validation
+    {
+        [Required(ErrorMessage="Title is required.")]
+        [StringLength(50,ErrorMessage="Only 50 characters allowed in Title")]
+        public string Title{get; set;}
+    }
+}
