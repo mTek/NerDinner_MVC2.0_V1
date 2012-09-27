a91a025b10b551d47c26accfd463c9a1c839b8b2
M Tek <mtek.helpdesk@gmail.com>
2012-09-27 08:14:41 +0100

    home

---
+++ Practice2/NerdDinner/NerdDinner.Tests/TEMP/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+namespace IEnumerable_IEnumerator.MyIEnumerable
+{
+    public class Employee
+    {
+        public Employee(string fName, string lName)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+        }
+        public string firstName;
+        public string lastName;
+    }
+    public class Employees<T> : IEnumerable<T>
+    {
+        List<T> empList = new List<T>();
+        public int Length { get { return empList.Count; } }
+        public void Add(T e)
+        {
+            empList.Add(e);
+        }
+        public void AddEmployee(T e)
+        {
+            empList.Add(e);
+        }
+        public T GetEmployee(int index)
+        {
+            return empList[index];
+        }
+        //IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        //{
+        //    return empList.GetEnumerator();
+        //}
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return empList.GetEnumerator();
+        //}
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return empList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return empList.GetEnumerator();
+        }
+    }
+}
+
